33caf6cbd51e8a4c0d222d09229f21b0e8281a13
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-05-18 18:07:09 +0100

    Chore: set up gemfile, file structure, begin writing tests

---
+++ lib/operator.rb
@@ -0,0 +1,5 @@
+class Operator
+  def initialize
+  end
+
+end
---
+++ spec/operator_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/operator.rb'
+
+describe 'Operator' do
+  before(:all) do
+    step_one = double("bracket")
+    step_two = double("bracket")
+    step_three = double("bracket")
+  end
+  
+  it 'runs operations' do
+
+  end
+
+  it 'only runs operations with certain conditions' do
+
+  end
+
+end
