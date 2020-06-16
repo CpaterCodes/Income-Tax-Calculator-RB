c31b9604fd8e4c6ad1ad1a4b4f8f96faa4e5b625
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-06-16 10:20:45 +0100

    Test: Stubs fully configured, failing test for calculate method

--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -2,18 +2,21 @@ require_relative '../lib/operator.rb'
 
 describe 'Operator' do
   before(:all) do
-    step_one = double("bracket")
-    step_two = double("bracket")
-    step_three = double("bracket")
-    steps = [step_one, step_two, step_three]
-    operator = Operator.new(steps)
+    @step_one = double("bracket")
+
+    @step_two = double("bracket")
+
+    @step_three = double("bracket")
+
+    @steps = [@step_one, @step_two, @step_three]
+    @operator = Operator.new(@steps)
   end
 
-  it 'runs operations' do
-
+  it 'runs operations and calculates total' do
+    allow(@step_one).to receive(:run).with(123).and_return(1)
+    allow(@step_two).to receive(:run).with(123).and_return(2)
+    allow(@step_three).to receive(:run).with(123).and_return(3)
+    expect @operator.calculate(123).to eq(6)
   end
 
-  it 'only runs operations with certain conditions' do
-
-  end
 end
