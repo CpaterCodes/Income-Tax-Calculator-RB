fe38a618130642bdd41fe48e36ebe5a81ee25d16
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-06-22 14:55:37 +0100

    Feat: Pass first test. Initialise bracket class files. Must consider test validity and good OOP principles

--- lib/operator.rb
+++ lib/operator.rb
@@ -1,5 +1,14 @@
 class Operator
   def initialize(brackets)
     @brackets = brackets
+    @total = 0
   end
+
+  def calculate(amount)
+    @brackets.each do |b|
+      @total += b.run(amount)
+    end
+  return @total
+  end
+
 end
--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -16,7 +16,7 @@ describe 'Operator' do
     allow(@step_one).to receive(:run).with(123).and_return(1)
     allow(@step_two).to receive(:run).with(123).and_return(2)
     allow(@step_three).to receive(:run).with(123).and_return(3)
-    expect @operator.calculate(123).to eq(6)
+    expect(@operator.calculate(123)).to eq(6)
   end
 
 end
