0e9f39fad874a818e054739ef6b5d917ae1e7d64
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-09-04 17:40:29 +0100

    tweak: Make bracket and bracket spec use float data type

--- lib/bracket.rb
+++ lib/bracket.rb
@@ -1,6 +1,10 @@
 class Bracket
-  def initialize(floor: nil, ceiling: nil)
+  def initialize(floor: nil, ceiling: nil, rate: 0)
     @floor = floor
     @ceiling = ceiling
+  end
+
+  def run(sum)
+    return "run! #{sum}"
   end
 end
--- spec/bracket_spec.rb
+++ spec/bracket_spec.rb
@@ -2,12 +2,12 @@ require_relative '../lib/bracket.rb'
 
 describe 'Bracket' do
   before(:all) do
-    @basic_rate = Bracket.new(floor: 12500, ceiling: 50000)
-    @higher_rate = Bracket.new(floor: 50000, ceiling: 150000)
+    @basic_rate = Bracket.new(floor: 12500.0, ceiling: 50000.0, rate:0.2)
+    @higher_rate = Bracket.new(floor: 50000.0, ceiling: 150000.0, rate:0.4)
   end
 
   it "Should return the rate of tax on a given bracket" do
-    expect(@basic_rate.run(15000)).to eq(500)
-    expect(@higher_rate.run(200000)).to eq(40000)
+    expect(@basic_rate.run(15000.0)).to eq(500.0)
+    expect(@higher_rate.run(200000.0)).to eq(40000.0)
   end
 end
