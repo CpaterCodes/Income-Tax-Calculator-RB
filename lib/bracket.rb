59925906c62081a7d3434ab0a345c9bc9d5c1370
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-09-04 17:57:22 +0100

    Feature: Build working code for basic and higher rate. Prepare to extract logic texts for code readability

--- lib/bracket.rb
+++ lib/bracket.rb
@@ -2,9 +2,13 @@ class Bracket
   def initialize(floor: nil, ceiling: nil, rate: 0)
     @floor = floor
     @ceiling = ceiling
+    @rate = rate
   end
 
   def run(sum)
-    return "run! #{sum}"
+    sum = @ceiling if @ceiling != nil && sum > @ceiling
+    sum -= @floor if @floor != nil
+    return sum * @rate
   end
+
 end
--- spec/bracket_spec.rb
+++ spec/bracket_spec.rb
@@ -2,11 +2,11 @@ require_relative '../lib/bracket.rb'
 
 describe 'Bracket' do
   before(:all) do
-    @basic_rate = Bracket.new(floor: 12500.0, ceiling: 50000.0, rate:0.2)
-    @higher_rate = Bracket.new(floor: 50000.0, ceiling: 150000.0, rate:0.4)
+    @basic_rate = Bracket.new(floor: 12500.0, ceiling: 50000.0, rate: 0.2)
+    @higher_rate = Bracket.new(floor: 50000.0, ceiling: 150000.0, rate: 0.4)
   end
 
-  it "Should return the rate of tax on a given bracket" do
+  it "Should return the basic and higher rates for given sums" do
     expect(@basic_rate.run(15000.0)).to eq(500.0)
     expect(@higher_rate.run(200000.0)).to eq(40000.0)
   end
