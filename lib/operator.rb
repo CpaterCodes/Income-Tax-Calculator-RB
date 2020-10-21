f8a465e13f89cb7c2aea22c39a584a992b528a54
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-10-21 17:44:52 +0100

    tweak: convert operator class to use static method

--- lib/operator.rb
+++ lib/operator.rb
@@ -1,15 +1,12 @@
 class Operator
-  def initialize(brackets)
-    @brackets = brackets
-    @total = 0
-  end
 
-  def calculate(amount)
-    @brackets.each do |b|
+  def self.calculate(brackets: nil, amount: 0)
+    total = 0
+    brackets.each do |b|
       rate = b.run(amount)
-      @total += rate if rate >= 0
+      total += rate if rate >= 0
     end
-  return @total
+  return total
   end
 
 end
--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -9,21 +9,20 @@ describe 'Operator' do
     @step_three = double("bracket")
 
     @steps = [@step_one, @step_two, @step_three]
-    @operator = Operator.new(@steps)
   end
 
   it 'runs operations and calculates total' do
     allow(@step_one).to receive(:run).with(123).and_return(1)
     allow(@step_two).to receive(:run).with(123).and_return(2)
     allow(@step_three).to receive(:run).with(123).and_return(3)
-    expect(@operator.calculate(123)).to eq(6)
+    expect(Operator.calculate(brackets: @steps, amount: 123)).to eq(6)
   end
 
   it 'does not allow for negative taxation' do
     allow(@step_one).to receive(:run).with(123).and_return(-2)
     allow(@step_two).to receive(:run).with(123).and_return(3)
     allow(@step_three).to receive(:run).with(123).and_return(1)
-    expect(@operator.calculate(123)).to eq(4)
+    expect(Operator.calculate(brackets: @steps, amount: 123)).to eq(4)
   end
 
 end
