66e912a4ea62e12b819767eb8a833bc86e3c932d
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-10-23 11:46:31 +0100

    Tweak: shorten calculate method

--- lib/operator.rb
+++ lib/operator.rb
@@ -2,11 +2,9 @@ class Operator
 
   def self.calculate(brackets: nil, amount: 0)
     total = 0
-    brackets.each do |b|
-      rate = b.run(amount)
-      total += rate if rate >= 0
+    brackets.each do |bracket|
+      total += bracket.run(amount) if bracket.run(amount) >= 0
     end
-  return total
+    total
   end
-
 end
